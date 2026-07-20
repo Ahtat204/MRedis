f5257ec92b64a93444fdafc8b2331e002b62fd7f
Lahcen <lahcen28ahtat@gmail.com>
2026-07-20 20:19:53 +0100

    implementing a ring buffer for reading TCP streams

---
+++ ringbuffer.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define RINGBUFFER_SIZE 1024
+
+typedef struct 
+{
+    char buffer[RINGBUFFER_SIZE];
+    char *read;
+    char *write;
+}ringbuffer;
+
+char readData(ringbuffer* ringbuffer)
+{
+    
+}
+char writeData(ringbuffer* ringbuffer,char character)
+{
+    
+}
+
