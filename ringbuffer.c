144dc3795a033f8f8eb53143475e6b0481f5a657
Lahcen <lahcen28ahtat@gmail.com>
2026-07-28 13:07:31 +0100

    first basic implementation of a ring buffer

--- ringbuffer.c
+++ ringbuffer.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #define RINGBUFFER_SIZE 1024
 
@@ -8,14 +9,33 @@ typedef struct
     char buffer[RINGBUFFER_SIZE];
     char *read;
     char *write;
+    size_t size;
 }ringbuffer;
 
-char readData(ringbuffer* ringbuffer)
+void init(ringbuffer *rb)
 {
-    
+    rb->write = rb->read=&(rb->buffer[0]);
+    rb->size = 0;
 }
-char writeData(ringbuffer* ringbuffer,char character)
+char readData(ringbuffer* rb)
 {
-    *(ringbuffer->write)= character;
+    if (rb->size==0) return '\0';
+    const char data=(rb->buffer[*(rb->read)]);
+    *(rb->read)=*(rb->read+1)%RINGBUFFER_SIZE;
+    rb->size--;
+    return data;
+}
+uint8_t writeData(ringbuffer* b,const char character)
+{
+   if (b->size==RINGBUFFER_SIZE)
+   {
+       printf(stderr);
+       return 0;
+   }
+    b->buffer[*b->write]=character;
+    *b->write=(*b->write+1)%RINGBUFFER_SIZE;
+    b->size++;
+    return 1;
+    
 }
 
